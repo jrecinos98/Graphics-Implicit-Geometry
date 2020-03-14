0f33699947ed2a77019bf5606a0875af9c97d005
Jose C <26911841+jrecinos98@users.noreply.github.com>
2020-03-13 20:30:07 -0700

    Update pieces.c

--- pieces.c
+++ pieces.c
@@ -114,9 +114,7 @@ float opSmoothIntersection( float d1, float d2, float k ) {
     float h = clamp( 0.5 - 0.5*(d2-d1)/k, 0.0, 1.0 );
     return mix( d2, d1, h ) + k*h*(1.0-h); 
 }
-
 float opUnion( float d1, float d2 ) {  return min(d1,d2); }
-
 float opSubtraction( float d1, float d2 ) { return max(-d1,d2); }
 
 float sdRoundedCylinder( vec3 p, float ra, float rb, float h, vec3 offset )
